933e6cd9475aa06f592460891f6723f09d17f4e6
Elena Guidi <37119745+ElePan@users.noreply.github.com>
2021-11-22 18:21:38 +0100

    add comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
+//Los ficheros jsx sirven para prear componentes (estructuras complejas de DOM)
+
 const App = () => {
+    //El nombre TIENE que empezar por mayuscula, lo diferencia de las etiquetas clasicas de HTML
     return (
         <div>
             <h1>Hola mundo</h1>
